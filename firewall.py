ab11a22ef49707d3eb449d361645f1e7d8d54fac
mileslindheimer <mileslindheimer@gmail.com>
2014-11-09 21:01:12 -0800

    geoipdb loading

--- firewall.py
+++ firewall.py
@@ -21,7 +21,14 @@ class Firewall:
             else:
                 self.rules.append(line)
         f.close()
-
+        tmp = []
+        db = open("geoipdb.txt")
+        while True:
+            line = f.readline()
+            if line == "":
+                break
+            tmp.append(line.split())
+        self.geoipdb = self.build_bst(tmp)
 
         # TODO: Load the firewall rules (from rule_filename) here.
         #print 'I am supposed to load rules from %s, but I am feeling lazy.' % \
@@ -36,12 +43,15 @@ class Firewall:
         send = self.iface_int.send_ip_packet if pkt_dir == PKT_DIR_INCOMING else self.iface_ext.send_ip_packet
         for rule in self.rules:
             verdict, rule_type, ip, port = self.parse_rule(rule)
-            #if not self.ip_okay(packet_ip):
+            if not self.ip_okay(packet_ip):
             #    continue
             #if is_protocol(rule_type) and not self.port_ok(packet_port):
             #    continue
         send(pkt)
             #break
+    def ip_match(self, ip, pkt_ip):
+        #compare ip addresses
+        return ip == 'any' or ip == self.geoipdb.lookup(pkt_ip).country() or ip == pkt_ip
 
     def parse_rule(self, rule):
         rule = rule.split()
