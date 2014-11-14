5fe6c1dcf9d1c0d9d4fda2caa842ea2ea7221112
Pierce <pierce.mt92@gmail.com>
2014-11-13 16:17:22 -0800

    Psuedocode for b-search, basic ip_addr unpacking

--- firewall.py
+++ firewall.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 from main import PKT_DIR_INCOMING, PKT_DIR_OUTGOING
+import socket
+import struct
 
 # TODO: Feel free to import any Python standard moduless as necessary.
 # (http://docs.python.org/2/library/)
@@ -26,25 +28,36 @@ class Firewall:
             line = db.readline()
             if line == "":
                 break
-            self.geoipdb.append(line.split())
+            line = line.split()
+            country, base, bound = line[2], struct.unpack('!L', socket.inet_aton(line[0])),
+                                   struct.unpack('L', socket.inet_aton(line[1]))
+            self.geoipdb.append((country, base, bound))
         db.close()
 
     # @pkt_dir: either PKT_DIR_INCOMING or PKT_DIR_OUTGOING
     # @pkt: the actual data of the IPv4 packet (including IP header)
     def handle_packet(self, pkt_dir, pkt):
         send = self.iface_int.send_ip_packet if pkt_dir == PKT_DIR_INCOMING else self.iface_ext.send_ip_packet
-        for rule in self.rules:
-            verdict, rule_type, ip, port = self.parse_rule(rule)
-            if not self.ip_okay(packet_ip):
-            #    continue
-            #if is_protocol(rule_type) and not self.port_ok(packet_port):
-            #    continue
-        send(pkt)
-            #break
+        pkt_protocol = struct.unpack('!B', pkt[9:10])
+        src_ip = socket.htonl(struct.unpack('!L', pkt[12:16]))
+        dst_ip = socket.htonl(struct.unpack('!L', pkt[16:20]))
+        pass
 
-    def ip_match(self, ip, pkt_ip):
-        #compare ip addresses
-        return ip == 'any' or ip == self.geoipdb.lookup(pkt_ip).country() or ip == pkt_ip
+        #for rule in self.rules:
+        #    verdict, rule_type, ip, port = self.parse_rule(rule)
+        #    if not self.ip_okay(packet_ip):
+        #    #    continue
+        #    #if is_protocol(rule_type) and not self.port_ok(packet_port):
+        #    #    continue
+        #send(pkt)
+        #    #break
+
+    def ip_match(self, rule_ip, pkt_ip):
+        # check if rule_ip is a country or just a regular address
+        # if it's a country, do binary search on pkt_ip (country, base, bound) record
+            # if the country matches that of the search record, the ip matches
+        # else check if the rule_ip matches the pkt_ip
+        
 
     def parse_rule(self, rule):
         rule = rule.split()
