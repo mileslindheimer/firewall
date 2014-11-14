4dc3877a4d53b6f0c99cf1c709c11626d9179694
Pierce <pierce.mt92@gmail.com>
2014-11-13 17:11:52 -0800

    Fixed function args .

--- firewall.py
+++ firewall.py
@@ -34,7 +34,6 @@ class Firewall:
             self.geoipdb.append((country, base, bound))
         db.close()
 
-
     def ip_match(self, rule_ip, pkt_ip):
         # check if rule_ip is a country or just a regular address
         # if it's a country, do binary search on pkt_ip (country, base, bound) record
@@ -50,11 +49,15 @@ class Firewall:
         return verdict, protocol_or_dns, ip, port
 
     def rule_matches(rule, pkt):
-        _, protocol_or_dns, ip, port = self.parse_rule(rule)
-        pkt_protocol = struct.unpack('!B', pkt[9:10])
+        _, prot_type, rule_ip, rule_port = self.parse_rule(rule)
+        pkt_prot = struct.unpack('!B', pkt[9:10])
         src_ip = socket.htonl(struct.unpack('!L', pkt[12:16]))
         dst_ip = socket.htonl(struct.unpack('!L', pkt[16:20]))
-        return self.ip_match(rule, src_ip) and self.ip_match(dst_ip)
+        
+        return (self.ip_match(prot_type, rule_ip, src_ip)
+                and self.ip_match(prot_type, rule_ip, dst_ip)
+                and self.prot_type_match(prot_type, pkt_prot)
+                and self.port_match(rule_port, pkt_port))
 
     # @pkt_dir: either PKT_DIR_INCOMING or PKT_DIR_OUTGOING
     # @pkt: the actual data of the IPv4 packet (including IP header)
