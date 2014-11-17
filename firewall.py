4c4ecbbf9e619ce68e37a33e13a8c14fb8cdb9c6
Pierce <pierce.mt92@gmail.com>
2014-11-16 19:42:06 -0800

    Working DNS.

--- firewall.py
+++ firewall.py
@@ -15,7 +15,7 @@ class Firewall:
         self.iface_int = iface_int
         self.iface_ext = iface_ext
         f = open(config['rule'])
-        self.ip_rules = []
+        self.rules = []
         self.geoipdb = []
         while True:
             line = f.readline()
@@ -24,10 +24,7 @@ class Firewall:
             elif line[0] == "%" or line[0] == "\n":
                 continue
             else:
-                line = self.parse_rule(line)
-                if line[2] == 'dns' and line[3] != 'any':
-                    line[3] = re.compile(self.regex_transform(line[3]))
-                self.rules.append(line)
+                self.rules.append(self.parse_rule(line))
         f.close()
         db = open("geoipdb.txt")
         while True:
@@ -42,11 +39,13 @@ class Firewall:
         db.close()
 
     def regex_transform(self, domain):
-        if domain[0] == "*":
-            return "\w*" + regex_transform(domain[1:])
+        if len(domain) == 1:
+            return domain
+        elif domain[0] == "*":
+            return "\w*" + self.regex_transform(domain[1:])
         elif domain[0] == ".":
-            return "\." + regex_transform(domain[1:])
-        return domain[0] + regex_transform(domain[1:])
+            return "\." + self.regex_transform(domain[1:])
+        return domain[0] + self.regex_transform(domain[1:])
 
     def bin_search(self, arr, v):
         if len(arr) == 0:
@@ -70,36 +69,48 @@ class Firewall:
             protocol_or_dns = 1
         else:
             protocol_or_dns = 'dns'
-        try:
-            ip = struct.unpack('!L', socket.inet_aton(rule[2]))
-        except socket.error:
-            ip = rule[2]
+        if protocol_or_dns == 'dns':
+            ip = re.compile(self.regex_transform(rule[2]))
+        else:
+            try:
+                ip = struct.unpack('!L', socket.inet_aton(rule[2]))
+            except socket.error:
+                ip = rule[2]
         port = None if len(rule) < 5 else int(rule[3])
         return (verdict, protocol_or_dns, ip, port)
 
     # unpack dns needs domain at the end of tuple
-    def unpack_packet(self, pkt, pkt_dir):
-        head_length = ord(pkt[:1]) & 0b00001111
-        if head_length < 5:
+    def unpack_pkt(self, pkt, pkt_dir):
+        head_length = 4 * (ord(pkt[:1]) & 0b00001111)
+        if head_length < 20:
             return None
         protocol = struct.unpack('!B', pkt[9:10])[0]
         if pkt_dir == PKT_DIR_INCOMING:
             ip = struct.unpack('L', pkt[12:16])[0]
-            port = struct.unpack('!L', pkt[head_length:(head_length + 4)])[0]
+            port = struct.unpack('!H', pkt[head_length:(head_length + 2)])[0]
         else:
             ip = struct.unpack('!L', pkt[16:20])[0]
-            port = struct.unpack('!L', pkt[(head_length + 4):(head_length + 8)])[0]
+            port = struct.unpack('!H', pkt[(head_length + 2):(head_length + 4)])[0]
         if port == 53 and protocol == 17:
-            qdcount = struct.unpack('!H', pkt[(head_length + 12):(head_length + 14)])
+            qdcount = struct.unpack('!H', pkt[(head_length + 12):(head_length + 14)])[0]
             if qdcount == 1:
-                i = 20
-                char = pkt[(head_length + i)]
+                ext_length = ord(pkt[head_length + 20])
+                base = head_length + 21
+                i = 0
                 domain = ""
-                while char != "":
-                    i += 1
-                    domain += char
-                    char = pkt[(head_length + i)]
-                return (head_length, protocol, ip, port, domain)
+                while pkt[base + i] != '\x00':
+                    if i == ext_length:
+                        ext_length, base = ord(pkt[base + i]), base + ext_length + 1
+                        domain += "."
+                        i = 0
+                    else:
+                        domain += pkt[base + i]
+                        i += 1
+                i += 1
+                q_type = struct.unpack('!H', pkt[base + i: base + i + 2])[0]
+                q_class = struct.unpack('!H', pkt[base + i + 2:base + i + 4])[0]
+                if (q_type == 1 or q_type == 28) and q_class == 1:
+                    return (head_length, protocol, ip, port, domain)
             return None
         return (head_length, protocol, ip, port)
 
@@ -111,19 +122,19 @@ class Firewall:
     def rule_matches(self, rule, pkt, pkt_dir, verdict):
         if rule[1] == 'dns' and len(pkt) == 5 and rule[2].match(pkt[4]) is not None:
             return rule[0]
-        match = (self.protocol_match(rule[1], pkt[1])
-                 and rule[2] == pkt[2]
-                 and rule[3] == pkt[3])
+        match = ((rule[1] == 'any' or rule[1] == pkt[1])
+                 and self.ip_match(rule[2], pkt[2])
+                 and rule[3] == 'any' or rule[3] == pkt[3])
         return rule[0] if match else verdict
 
     # @pkt_dir: either PKT_DIR_INCOMING or PKT_DIR_OUTGOING
     # @pkt: the actual data of the IPv4 packet (including IP header)
     def handle_packet(self, pkt_dir, pkt):
-        unpacked_pkt = self.unpack_pkt(pkt)
+        unpacked_pkt = self.unpack_pkt(pkt, pkt_dir)
         verdict = 'pass'
         if unpacked_pkt is not None:
             for rule in self.rules:
-                verdict = self.rule_matches(rule, pkt, pkt_dir, verdict)
+                verdict = self.rule_matches(rule, unpacked_pkt, pkt_dir, verdict)
             if verdict == 'pass':
                 if pkt_dir == PKT_DIR_INCOMING:
                     self.iface_int.send_ip_packet(pkt)
